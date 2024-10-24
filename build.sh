9f698c8eed887ad58700abfef5e29b4a39e26ad6
Zhu Hengbo <zhuhengbo@iscas.ac.cn>
2024-10-24 07:57:26 +0000

    Update linux compile Makefile
    Signed-off-by: Zhu Hengbo <zhuhengbo@iscas.ac.cn>

--- build.sh
+++ build.sh
@@ -1,6 +1,40 @@
 
 
 
+REALEASE_TYPE:= Debug
+TARGET_DIR = build/$(REALEASE_TYPE)
+
+create_build_dir()
+{
+    echo "Creating target directory..."
+    mkdir -p "${TARGET_DIR}"
+    if [ $? -eq 0 ]; then
+        echo "Target directory created successfully: ${TARGET_DIR}"
+    else
+        echo "Failed to create target directory: ${TARGET_DIR}"
+        exit 1
+    fi
+}
+
+brs_build()
+{
+
+}
 
 
+brs_install()
+{
 
+}
+
+
+main()
+{
+    create_build_dir()
+
+    brs_build()
+    brs_install()
+}
+
+
+main
